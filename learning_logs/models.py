be5258fa4aa8e257c6357a772a6af65fc1b55a3a
Tionni <johorali2544@gmail.com>
2024-09-30 12:03:05 +0300

    Set DEBUG False on live site.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -22,4 +22,6 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a simple string representing the entry."""
+        if len(self.text) <50:
+            return self.text
         return f"{self.text[:50]}..."    
